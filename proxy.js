7f67db95252279a77a97d5ef383688beab80954a
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-06-03 00:06:58 +0100

    Create proxy.js

---
+++ proxy.js
@@ -0,0 +1,23 @@
+const https = require('https');
+
+const options = {
+  hostname: 'api.cloudinary.com',
+  path: '/v1_1/duldfki6j/resources/image',
+  method: 'GET'
+};
+
+const req = https.request(options, (res) => {
+  let data = '';
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    console.log(data);
+  });
+});
+
+req.on('error', (error) => {
+  console.error(error);
+});
+
+req.end();
