5d3a2d9c6402020d0a2efb02aa102dbed4ba8d07
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-06-02 02:30:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 const cloudName = 'duldfki6j';
 const apiKey = '474577221937364';
 const apiSecret = 'NdmEurAKxUGT-W_wuS7OhoYdi8k';
+const uploadPreset = 'new-preset';
 const cl = cloudinary.Cloudinary.new({ cloud_name: cloudName, secure: true });
+
+// Set the delivery URL for accessing media files
+const deliveryURL = 'https://res.cloudinary.com/duldfki6j';
+
+// Set the API base URL for making API requests
+const apiBaseURL = 'https://api.cloudinary.com/v1_1/duldfki6j';
 
 // Get the container element for the image grid
 const imageGrid = document.getElementById('image-grid');
@@ -11,9 +18,9 @@ const imageGrid = document.getElementById('image-grid');
 cl.api.resources({ type: 'upload', max_results: 20 }, (error, result) => {
   if (error) {
     console.error('Failed to fetch images:', error);
-  } else {
+  } else if (result && result.resources) {
     result.resources.forEach((resource) => {
-      const imageUrl = resource.secure_url;
+      const imageUrl = `https://res.cloudinary.com/${cloudName}/image/upload/${resource.public_id}.${resource.format}`;
 
       // Create an image element and set the source
       const image = document.createElement('img');
@@ -22,7 +29,27 @@ cl.api.resources({ type: 'upload', max_results: 20 }, (error, result) => {
       // Append the image to the image grid container
       imageGrid.appendChild(image);
     });
+  } else {
+    console.error('Invalid response format:', result);
   }
 });
 
+// Add an event listener to the save button
+const saveButton = document.getElementById('save-button');
+saveButton.addEventListener('click', saveImage);
 
+// Function to handle saving the image
+function saveImage() {
+  // Convert the p5.js canvas content to a data URL
+  const canvas = window.canvas; // Replace "canvas" with the variable storing your p5.js canvas
+  const imageData = canvas.elt.toDataURL('image/png');
+
+  // Upload the image to Cloudinary
+  cl.upload(imageData, { upload_preset: uploadPreset }, (error, result) => {
+    if (error) {
+      console.error('Failed to save the image.', error);
+    } else {
+      console.log('Image saved on Cloudinary:', result.secure_url);
+    }
+  });
+}
