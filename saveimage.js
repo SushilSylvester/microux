a08d4423f60ed059d43a14dc8d6091aa5fd46bc4
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-06-02 02:24:01 +0100

    Update saveimage.js

--- saveimage.js
+++ saveimage.js
@@ -2,7 +2,9 @@
 const cloudName = 'duldfki6j';
 const apiKey = '474577221937364';
 const apiSecret = 'NdmEurAKxUGT-W_wuS7OhoYdi8k';
-const cl = cloudinary.Cloudinary.new({ cloud_name: cloudName, secure: true });
+
+// Set the API base URL for making API requests
+const apiBaseURL = 'https://api.cloudinary.com/v1_1/duldfki6j';
 
 // Add an event listener to the save button
 const saveButton = document.getElementById('save-button');
@@ -10,16 +12,26 @@ saveButton.addEventListener('click', saveImage);
 
 // Function to handle saving the image
 function saveImage() {
-  // Convert the canvas content to a data URL
+  // Convert the p5.js canvas content to a data URL
   const canvas = document.getElementById('defaultCanvas0');
   const imageData = canvas.toDataURL('image/png');
 
   // Upload the image to Cloudinary
-  cl.upload(imageData, (error, result) => {
-    if (error) {
-      console.error('Failed to save the image.');
-    } else {
+  fetch(`${apiBaseURL}/upload`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      file: imageData,
+      upload_preset: 'new-preset',
+    }),
+  })
+    .then(response => response.json())
+    .then(result => {
       console.log('Image saved on Cloudinary:', result.secure_url);
-    }
-  });
+    })
+    .catch(error => {
+      console.error('Failed to save the image:', error);
+    });
 }
