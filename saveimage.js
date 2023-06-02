c05eeb859b3b47085e54cd1dfbe106e9781e374c
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-06-02 03:39:02 +0100

    Update saveimage.js

--- saveimage.js
+++ saveimage.js
@@ -20,7 +20,7 @@ function saveImage() {
   const formData = new FormData();
   formData.append('file', imageData);
 
-  fetch('https://api.cloudinary.com/v1_1/your_cloud_name/upload', {
+  fetch('https://api.cloudinary.com/v1_1/duldfki6j/upload', {
     method: 'POST',
     body: formData
   })
