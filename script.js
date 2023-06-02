87007e2b7f80480c568010a3e33d5c999b7537b1
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-06-02 02:59:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-// Configure Cloudinary
-const cloudName = 'duldfki6j';
-const deliveryURL = `http://res.cloudinary.com/duldfki6j`;
-
-// Get the container element for the image grid
-const imageGrid = document.getElementById('image-grid');
-
-// Fetch the image URLs from Cloudinary and create the image elements
-fetch(`${deliveryURL}/image/list/sample.json`, { mode: 'cors' })
-  .then(response => response.json())
-  .then(data => {
-    if (data.error) {
-      console.error('Failed to fetch images:', data.error.message);
-    } else {
-      data.resources.forEach((resource) => {
-        const imageUrl = `${deliveryURL}/image/upload/${resource.public_id}.${resource.format}`;
-
-        // Create an image element and set the source
-        const image = document.createElement('img');
-        image.src = imageUrl;
-
-        // Append the image to the image grid container
-        imageGrid.appendChild(image);
+// Check if the current page is gallery.html before fetching images
+if (window.location.pathname.includes('gallery.html')) {
+    // Configure Cloudinary
+    const cloudName = 'duldfki6j';
+    const deliveryURL = `http://res.cloudinary.com/duldfki6j`;
+  
+    // Get the container element for the image grid
+    const imageGrid = document.getElementById('image-grid');
+  
+    // Fetch the image URLs from Cloudinary and create the image elements
+    fetch(`${deliveryURL}/image/list/sample.json`)
+      .then(response => response.json())
+      .then(data => {
+        data.resources.forEach(resource => {
+          const imageUrl = `${deliveryURL}/image/upload/${resource.public_id}.${resource.format}`;
+  
+          // Create an image element and set the source
+          const image = document.createElement('img');
+          image.src = imageUrl;
+  
+          // Append the image to the image grid container
+          imageGrid.appendChild(image);
+        });
+      })
+      .catch(error => {
+        console.error('Failed to fetch images:', error);
       });
-    }
-  })
-  .catch(error => {
-    console.error('Failed to fetch images:', error);
-  });
+  }  
