1941c29a99ff494d702da5d54c39850d91780178
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-06-03 00:28:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,26 @@ async function fetchImages() {
 
 // Fetch and display the images when the page is loaded
 window.addEventListener('load', fetchImages);
+
+async function fetchImages() {
+  try {
+    const response = await fetch(proxyUrl);
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // Log the response data
+      const images = data.resources;
+
+      // Generate HTML for each image and append it to the gallery container
+      images.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.src = image.secure_url;
+        galleryContainer.appendChild(imgElement);
+      });
+    } else {
+      console.error('Failed to fetch images:', response.status, response.statusText);
+    }
+  } catch (error) {
+    console.error('Failed to fetch images:', error);
+  }
+}
