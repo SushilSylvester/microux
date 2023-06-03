9d1f0474cb10502db6bb33d6473db531a613f4b0
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-06-03 01:31:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const proxyUrl = 'https://connectloop.netlify.app/proxy.js'; // Update with the 
 
 // Function to fetch images from Cloudinary and display them in the gallery
 async function fetchImages() {
-  const cloudinary = new Cloudinary({ cloud_name: cloudName, secure: true });
+  const cloudinary = new cloudinary.Cloudinary({ cloud_name: cloudName, secure: true });
 
   try {
     const { resources } = await cloudinary.api.resources({ type: 'upload' });
