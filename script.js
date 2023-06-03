f2ddd72d7dfe16270813ca0881fde8073d7f15fc
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-06-03 15:52:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,33 @@ const apiKey = '474577221937364';
 const apiSecret = 'NdmEurAKxUGT-W_wuS7OhoYdi8k';
 //const proxyUrl = 'https://connectloop.netlify.app/proxy.js'; // Update with the URL to your server-side proxy
 
-// Replace 'YOUR_CLOUD_NAME' with your Cloudinary cloud name
-const url = `https://res.cloudinary.com/${cloudName}/image/upload/all.json`;
+// Fetch image list from Cloudinary using the Cloudinary SDK
+cloudinary.v2.api.resources(
+  { type: 'upload', prefix: 'your-prefix-if-any/', max_results: 10 },
+  (error, result) => {
+    if (error) {
+      console.log('Error:', error);
+      return;
+    }
 
-// Fetch image list from Cloudinary
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const imageGrid = document.getElementById('imageGrid');
-        
-        // Iterate through each image in the list
-        data.resources.forEach(image => {
-            // Create an <img> element for each image
-            const img = document.createElement('img');
-            img.src = image.url;
-            
-            // Append the image to the grid container
-            imageGrid.appendChild(img);
-        });
-    })
-    .catch(error => {
-        console.log('Error:', error);
+    const imageGrid = document.getElementById('image-grid');
+
+    // Iterate through each image in the list
+    result.resources.forEach(image => {
+      // Generate the URL for the image using the Transformation API
+      const imageUrl = cloudinary.url(image.public_id, {
+        cloud_name: cloudName,
+        width: 300,
+        height: 300,
+        crop: 'fill'
+      });
+
+      // Create an <img> element for each image
+      const img = document.createElement('img');
+      img.src = imageUrl;
+
+      // Append the image to the grid container
+      imageGrid.appendChild(img);
     });
+  }
+);
