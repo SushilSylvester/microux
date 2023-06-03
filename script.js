8d60749111f6dcb1f94df9a33df141cfbaf2e18c
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-06-03 01:19:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // Configure Cloudinary
 const cloudName = 'duldfki6j';
+const apiKey = '474577221937364';
+const proxyUrl = 'https://connectloop.netlify.app/proxy.js'; // Update with the URL to your server-side proxy
 
 // Function to fetch images from Cloudinary and display them in the gallery
 async function fetchImages() {
   try {
-    const response = await fetch(`${proxyUrl}?api_key=${474577221937364}`, {
+    const response = await fetch(`${proxyUrl}?api_key=${apiKey}`, {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${474577221937364}`
+        'Authorization': `Bearer ${apiKey}`
       },
       mode: 'cors'
     });
@@ -30,7 +32,6 @@ async function fetchImages() {
     console.error('Failed to fetch images:', error);
   }
 }
-
 
 // Fetch and display the images when the page is loaded
 window.addEventListener('load', fetchImages);
